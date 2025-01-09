5058ae605474bdc7addbff0f6748833fd42b18f4
Yuvraj Negi <yashn2393@gmail.com>
2025-01-09 21:59:10 +0530

    ApnaPay

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        deepblue: "#02042a",
+        lightblue: "#2b84ea",
+        lightblue300: "#4b943e",
+        lightblue500: "#0b72e7",
+        greenlight: "#61cea5",
+        graytext: "#818597",
+        lightgray: "#e2e2e2",
+        grayblue: "#344a6c",
+        deepbluehead: "#162f56",
+        gray2: "#525a76",
+      },
+    },
+  },
+  plugins: [],
+};
